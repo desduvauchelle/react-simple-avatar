c9a0c543574475ae6813764d5b4121ecc463ef3e
Denis Duvauchelle <desduvauchelle@users.noreply.github.com>
2021-01-10 13:09:25 -0800

    Updates

---
+++ precache-manifest.9f77abea4bf95a445ccb8faae3e4206a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba701a3a665c17d2b2c2cb8c1054b44f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "48ffca6473d860c2c965",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "7cc770037ca0db0daf87",
+    "url": "./static/js/2.164cfd8a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.164cfd8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ffca6473d860c2c965",
+    "url": "./static/js/main.49eaeb68.chunk.js"
+  },
+  {
+    "revision": "0b4fb701295ea48ec854",
+    "url": "./static/js/runtime-main.ae59e5ca.js"
+  }
+]);
